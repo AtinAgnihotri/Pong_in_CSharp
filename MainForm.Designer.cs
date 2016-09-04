c798c908f124c58f29205b70bcaf347ebca29e93
AtinAgnihotri <AtinAgnihotri@users.noreply.github.com>
2016-09-04 05:35:39 +0530

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Atin
+ * Date: 04-09-2016
+ * Time: 00:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Pong
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1615, 895);
+			this.Name = "MainForm";
+			this.Text = "Pong";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mouseClick_1);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouseMove_1);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,113 @@
+﻿/*
+A simple pong game using C#
+ */
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pong
+{
+
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();}
+			
+			Bitmap btm;
+			Graphics bGraphics;
+			Graphics sGraphics;
+			
+			Thread mth;
+			
+			Rectangle lPaddle = Rectangle.Empty;
+			Rectangle rPaddle = Rectangle.Empty;
+			Rectangle ball = Rectangle.Empty;
+			
+			//int ballSx, ballSy, paddleS;
+			
+			int ballSx = 1;
+			int ballSy = 1;
+			int paddleS = 5;
+			
+			Point movePaddle =  Point.Empty;
+			Point moveBall =  Point.Empty;
+			
+			bool looper = true;
+			
+			private void MainForm_Load (object sender, EventArgs e)
+			{
+				btm =  new Bitmap (this.Width, this.Height);
+				//btm =  new Bitmap (800, 600);
+				bGraphics = Graphics.FromImage(btm);
+				sGraphics = this.CreateGraphics();
+				
+				ball = new Rectangle(this.Width/2, this.Height/3, 40, 40);
+				rPaddle = new Rectangle((this.Width - 50), this.Height/3, 30, 150);
+				lPaddle = new Rectangle(5, this.Height/3, 30, 150);
+				
+				mth = new Thread(draw);
+				
+				mth.IsBackground = true;
+				
+				mth.Start();
+			}
+			
+			public void draw()
+			{
+				while(looper)
+				{
+					bGraphics.Clear(Color.White);
+					
+					if(movePaddle.Y > rPaddle.Y + 100) rPaddle.Y +=  paddleS;
+					
+					if(movePaddle.Y < rPaddle.Y + 100) rPaddle.Y -=  paddleS;
+					
+					if(ball.Y > lPaddle.Y + 100) lPaddle.Y +=  paddleS;
+					
+					if(ball.Y < lPaddle.Y + 100) lPaddle.Y -=  paddleS;
+
+					ball.X += ballSx;
+					
+					if(moveBall.Y > ball.Y) ball.Y +=  ballSy;
+					
+					if(moveBall.Y < ball.Y) ball.Y -=  ballSy;
+					
+					if((rPaddle.IntersectsWith(ball)) || (lPaddle.IntersectsWith(ball))) ballSx *= (-1);
+					
+					if(ball.Y < 20) moveBall.Y +=  this.Height;
+					
+					if(ball.Y < this.Height - 80 ) moveBall.Y -=  0;
+					
+					if(ball.Y < -40) ball.X +=  this.Width/2;
+					
+					if(ball.Y < this.Width ) ball.X +=  this.Width/2;
+
+					
+					
+					bGraphics.FillEllipse(Brushes.Black, ball);
+					bGraphics.FillRectangle(Brushes.Red, lPaddle);
+					bGraphics.FillRectangle(Brushes.Blue, rPaddle);
+					
+					sGraphics.DrawImage(btm, 0, 0, this.Width, this.Height);
+					
+					
+				}
+					
+			}
+			
+			private void mouseMove_1(object sender, MouseEventArgs e)
+			{
+				movePaddle = e.Location;
+			}
+			
+			private void mouseClick_1(object sender, MouseEventArgs e)
+			{
+				moveBall = e.Location;
+			}
+		}
+	}
+
